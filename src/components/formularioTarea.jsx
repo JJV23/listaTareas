dcd69b162a502c6653a4dc358838498bddac0a9b
Jennifer Valor <profe.jennifer.valor@gmail.com>
2024-02-21 17:47:55 -0300

    agregamos el componente FormularioTarea

--- src/components/formularioTarea.jsx
+++ src/components/formularioTarea.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-
-const formularioTarea = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import { Button, Form } from "react-bootstrap";
+//import Form from "react-bootstrap/Form";
+//import Form from "react-boostrap/Button";
+const FormularioTarea = () => { 
+  return (
+    <section>
+      <Form>
+        <Form.Group className="mb-3 d-flex" controlId="formularioTarea">
+          <Form.Control type="text" placeholder="Ej: tarea 1" className="me-2" />
+          <Button variant="dark" type="submit">Agregar</Button>
+        </Form.Group>
+        
+      </Form>
+    </section>
+  );
 };
 
-export default formularioTarea;
+export default FormularioTarea;
